ba00a4020adde6b6079d0f2b3ace003bd4d29599
Ian Avery <ianedavery@gmail.com>
2017-12-03 09:12:30 -0700

    initial commit

--- server.js
+++ server.js
@@ -37,6 +37,55 @@ app.get('/posts/:id', (req, res) => {
 		});
 });
 
+app.post('/posts', (req, res) => {
+	const requiredFields = ['title', 'author', 'content'];
+	for(let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if(!(field in req.body)) {
+			const message = `Missing ${field} in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	BlogPost
+		.create({
+			title: req.body.title,
+			author: req.body.author,
+			content: req.body.content
+		})
+		.then(post => res.status(201).json(post.apiRepr()))
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
+
+app.put('/posts/:id', (req, res) => {
+	if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		const message = (`Request path ID (${req.params.id}) and request body ID ` + `(${req.body.id}) must match`);
+		console.error(message);
+		return res.status(400).json({"message": message});
+	}
+	const toUpdate = {};
+	const updateableFields = ['title', 'author', 'content'];
+	updateableFields.forEach(field => {
+		if(field in req.body) {
+			toUpdate[field] = req.body[field];
+		}
+	});
+	BlogPost
+		.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+		.then(post => res.status(200).json(post.apiRepr()))
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+app.delete('/posts/:id', (req, res) => {
+	BlogPost
+		.findByIdAndRemove(req.params.id)
+		.then(res.status(204).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
 let server;
 
 function runServer(databaseUrl=DATABASE_URL, port=PORT) {
